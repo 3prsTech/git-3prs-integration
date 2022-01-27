97775b4b2972fb8fb8557719d7c3e03af2eb3ea1
Operations <88063113+3prsTech@users.noreply.github.com>
2022-01-27 13:08:30 -0500

    Update registerdevice_submit_javascript.js

--- js/registerdevice_submit_javascript.js
+++ js/registerdevice_submit_javascript.js
@@ -4,15 +4,18 @@ window.addEventListener( "load", function () {
 
     // Bind the FormData object and the form element
     const FD = new FormData( form );
+    const submitBtn = document.getElementById( "submitBtn" );
 
     // Define what happens on successful data submission
     XHR.addEventListener( "load", function(event) {
       alert( event.target.responseText );
+      submitBtn.disabled = false;
     } );
 
     // Define what happens in case of error
     XHR.addEventListener( "error", function( event ) {
       alert( 'Oops! Something went wrong.' );
+      submitBtn.disabled = false;
     } );
 
     // Set up our request
@@ -24,11 +27,12 @@ window.addEventListener( "load", function () {
 
   // Access the form element...
   const form = document.getElementById( "form_id" );
+  const submitBtn = document.getElementById( "submitBtn" );
 
   // ...and take over its submit event.
   form.addEventListener( "submit", function ( event ) {
     event.preventDefault();
-
+    submitBtn.disabled = true;
     sendData();
   } );
 } );
